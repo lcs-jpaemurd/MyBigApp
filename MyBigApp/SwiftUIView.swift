9af0e67c3c8a2f0b8b522760146dfe5c83a7882c
Joshua Paemurd <jpaemurd@lcs.on.ca>
2026-03-04 09:00:10 -0500

    Set up

---
+++ MyBigApp/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyBigApp
+//
+//  Created by Joshua Paemurd on 2026-03-04.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
