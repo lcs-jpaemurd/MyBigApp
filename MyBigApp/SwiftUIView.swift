67b67bd5a98b2c14b25435e73a6e500c197ba2f3
Joshua Paemurd <jpaemurd@lcs.on.ca>
2026-03-04 09:18:34 -0500

    Not much to see here

--- MyBigApp/SwiftUIView.swift
+++ MyBigApp/SwiftUIView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Rectangle()
+                .containerRelativeFrame(.vertical, count: 10, span: 3, spacing: 0)
+            HStack{
+                Text("2022 Tesla Model X")
+                    .font(.system(size: 26.0, weight: .bold, design: .default))
+                    .padding(26)
+                Spacer()
+            }
+            HStack{
+                Text("$95 000 000")
+                    .font(.system(size: 20.0, weight: .semibold, design: .default)) //make this blue
+                    .padding(26)
+                Spacer()
+            }
+            
+            
+        }
     }
 }
 
